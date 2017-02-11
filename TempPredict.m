f5b0847701e9f265b8b8968545a747b310ea54de
Spencer Manning <spencerhmanning@gmail.com>
2017-02-11 10:57:01 -0700

    This is my code when Jdrive was down.

---
+++ TempPredict.m
@@ -0,0 +1,20 @@
+function [] = TempPredict()
+
+% This is just a temporary file to use until the Jdrive is back up
+F = []; % State transition matrix
+B = [];% The control input model
+P = []; % Initial error covariance matrix 
+Q = []; % Initial 
+I = eye(n);  % How many dimensions do I have?
+xhat = 1;  % Arbitrary selection
+for k = 1:5
+    % Predicted state estimate
+    if k >= k-1
+        F = 
+        B = 
+        u = 
+        xhat = 
+
+end
+
+end
